b677da7b149f1c7fb05c2395c2c427ddf7c23887
Deilon Cutamora <deilast80@gmail.com>
2019-12-06 14:29:38 +0800

    Sync profile settings form reactively

--- src/app/user/profile-update/profile-update.component.ts
+++ src/app/user/profile-update/profile-update.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms';
+import { User } from '../user.model';
 
 @Component({
   selector: 'app-profile-update',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileUpdateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  profileSettings: FormGroup;
+  authUser: User = JSON.parse(localStorage.getItem('authUser'));
 
   ngOnInit() {
+
+    this.profileSettings = this.fb.group({
+      'basic': this.fb.group({
+        'fullname': [this.authUser.name, [Validators.required, Validators.pattern(/^[ A-Za-z]([ A-Za-z]+)*$/)]],
+        'username': [this.authUser.username, Validators.required],
+        'website': [this.authUser.website],
+        'bio': [this.authUser.bio]
+      }),
+      'private': this.fb.group({
+        'phone': [this.authUser.phone],
+        'gender': [this.authUser.gender]
+      })
+    })
+
   }
 
 
