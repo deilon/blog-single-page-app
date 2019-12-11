87058c9ee04c97510285360de5411c6896639ca1
Deilon Cutamora <deilast80@gmail.com>
2019-12-12 00:48:51 +0800

    Adding userComponent to UsersModule declarations

--- src/app/modules/private/user/users.module.ts
+++ src/app/modules/private/user/users.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms'; 
 
+import { UserComponent } from './user.component';
 import { ProfileBlogsComponent } from './profile-blogs/profile-blogs.component';
 import { ProfileUpdateComponent } from './profile-update/profile-update.component';
 import { UserRouingModule } from './user-routing.module';
 
 @NgModule({
     declarations: [
+        UserComponent,
         ProfileBlogsComponent,
-        ProfileUpdateComponent,
+        ProfileUpdateComponent
     ],
     imports: [
         CommonModule,
